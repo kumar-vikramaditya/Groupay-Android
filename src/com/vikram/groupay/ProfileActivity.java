cb2212a5df3abb0d0160a12850a9711d80f5c5d0
nirali-tank <nirali.tank@sjsu.edu>
2015-04-30 13:50:07 -0700

    Updated the group notification

--- src/com/vikram/groupay/ProfileActivity.java
+++ src/com/vikram/groupay/ProfileActivity.java
@@ -1,8 +1,18 @@
 package com.vikram.groupay;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+
+
+
+
 
 
 
@@ -13,6 +23,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -36,12 +47,26 @@ public class ProfileActivity extends Activity{
 	Button groups,events;
 	SharedPreferences pref;
 	
+	 
 	ListView list_groups;
 	ListView list_events;
 	PopupWindow pwindow;
 	Button btnIgnoreGrp;
+	
+	Button btnAcceptGrp;
+	
+	
+	String[] groupName;
+	String[] groupMembers;
+	
+	
+	String[] groupAdmin;
+	ArrayAdapter<String> groupMembersAdapter;
 	ArrayAdapter<String> groupAdapter;
-	String[] groupNotifications;
+	List<NameValuePair> params1;
+	List<NameValuePair> params;
+	int globalPosition;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,30 +91,10 @@ public class ProfileActivity extends Activity{
         });
 		try {
 			user=new JSONObject(getIntent().getStringExtra("userdata"));
-			fisrtName.setText(user.getString("firstName"));
+			fisrtName.setText(user.getString("firstName").toUpperCase());
 			//Log.e("UserDetails",(new Integer(user.getJSONArray("groupRequests").length())).toString());
-			groupNotifications= new String[user.getJSONArray("groupRequests").length()];
-			//converJsonArrayToString(user.getJSONArray("groupRequests"),groupNotifications);
+			getGroupRequests();
 			
-			
-			for(int i = 0; i < user.getJSONArray("groupRequests").length(); i++) {
-				groupNotifications[i] = user.getJSONArray("groupRequests").getString(i);
-			}
-			//Log.e("UserDetails",groupNotifications[0]);
-			groupAdapter= new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_1, groupNotifications);
-			//groupAdapter.
-			list_groups.setAdapter(groupAdapter);
-			list_groups.setOnItemClickListener(new OnItemClickListener() {
-
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-					// TODO Auto-generated method stub
-					Log.e("list", "itemclicked");
-					getPopupWindow();
-				}
-			});
 			//groupNotifications=(user.getJSONArray("groupRequests").toString());
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -107,41 +112,155 @@ public class ProfileActivity extends Activity{
 					R.layout.activity_groupnotification,
 					(ViewGroup) findViewById(R.id.rel_popup));
 			// Log.e("grpmembers", layout.toString());
+			TextView grpName=(TextView)layout.findViewById(R.id.tv_grpname);
+			grpName.setText(groupName[globalPosition]);
+			
 			ListView grpmembers = (ListView) layout
 					.findViewById(R.id.list_grpmembers);
-			Log.e("grpmembers", grpmembers.toString());
-			String[] values2 = new String[] { "notification e 1",
-					"notification e 2", "notification e 2",
-					"notification e 2" };
-			ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+			
+			getGroupMembers();
+			
+           groupMembersAdapter = new ArrayAdapter<String>(
 					ProfileActivity.this,
-					android.R.layout.simple_list_item_1, values2);
+					android.R.layout.simple_list_item_1, groupMembers);
 			// Log.e(tag, msg);
-			grpmembers.setAdapter(adapter2);
-			pwindow = new PopupWindow(layout, 300, 250, true);
+			grpmembers.setAdapter(groupMembersAdapter);
+			Display disp=getWindowManager().getDefaultDisplay();
+			int width=disp.getWidth();
+			int height=disp.getHeight();
+			//layout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+			pwindow = new PopupWindow(layout, width-width/4,height-height/8, true);
 			pwindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
 			btnIgnoreGrp = (Button) layout
 					.findViewById(R.id.btnIgnoreGrp);
 			btnIgnoreGrp.setOnClickListener(button_ignore);
+			
+			btnAcceptGrp = (Button) layout
+					.findViewById(R.id.btnAcceptGrp);
+			btnAcceptGrp.setOnClickListener(btnAccept);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	private OnClickListener button_ignore = new OnClickListener() {
+	
+	private OnClickListener btnAccept = new OnClickListener() {
 
 		public void onClick(View view) {
 			// TODO Auto-generated method stub
+			
+            try {
+        	ServerRequest sr = new ServerRequest();
+		 	params = new ArrayList<NameValuePair>();
+           params.add(new BasicNameValuePair("groupName", groupName[globalPosition]));
+           params.add(new BasicNameValuePair("groupAdminEmail", groupAdmin[globalPosition]));
+			params.add(new BasicNameValuePair("userEmail", user.getString("email")));
+			 params.add(new BasicNameValuePair("updateAction", "Join Group"));
+	           JSONObject json = sr.getJSON(AppSettings.SERVER_IP+"/updateGroup",params);
+	           
+	           
+			 	params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("userEmail", user.getString("email")));
+		         JSONObject newjson = sr.getJSON(AppSettings.SERVER_IP+"/refreshUser",params);
+		         user=newjson.getJSONObject("user");
+		         
+		         getGroupRequests();
+	           //groupAdapter.notifyDataSetChanged();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+           
 			pwindow.dismiss();
 
 		}
 	};
 
 	
-	private void converJsonArrayToString(JSONArray jsonArray, String[] groupNotifications){
-		String jsonString = jsonArray.toString();
-		jsonString.replace("},{", " ,");
-		groupNotifications = jsonString.split(" ");
+	private OnClickListener button_ignore = new OnClickListener() {
+
+		public void onClick(View view) {
+			// TODO Auto-generated method stub
+			
+            try {
+        	ServerRequest sr = new ServerRequest();
+		 	params = new ArrayList<NameValuePair>();
+           params.add(new BasicNameValuePair("groupName", groupName[globalPosition]));
+           params.add(new BasicNameValuePair("groupAdminEmail", groupAdmin[globalPosition]));
+			params.add(new BasicNameValuePair("userEmail", user.getString("email")));
+			 params.add(new BasicNameValuePair("updateAction", "Ignore Group"));
+	           JSONObject json = sr.getJSON(AppSettings.SERVER_IP+"/updateGroup",params);
+	           
+	           
+			 	params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("userEmail", user.getString("email")));
+		         JSONObject newjson = sr.getJSON(AppSettings.SERVER_IP+"/refreshUser",params);
+		         user=newjson.getJSONObject("user");
+		         
+		         getGroupRequests();
+	           groupAdapter.notifyDataSetChanged();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+           
+			pwindow.dismiss();
+
+		}
+	};
+
+	
+	private void getGroupRequests() throws JSONException{
+		groupName= new String[user.getJSONArray("groupRequests").length()];
+		groupAdmin=new String[user.getJSONArray("groupRequests").length()];
+		//converJsonArrayToString(user.getJSONArray("groupRequests"),groupNotifications);
+		
+		
+		for(int i = 0; i < user.getJSONArray("groupRequests").length(); i++) {
+			ServerRequest sr = new ServerRequest();
+			 params = new ArrayList<NameValuePair>();
+             params.add(new BasicNameValuePair("groupToken", user.getJSONArray("groupRequests").getString(i)));
+            JSONObject json = sr.getJSON(AppSettings.SERVER_IP+"/getGroupDetails",params);
+            if(json != null){
+           // try{
+            	//String jsonstr = json.getString("response");
+            	groupName[i]=json.getString("groupName");
+            	groupAdmin[i]=json.getString("groupAdmin");
+            //}catch (JSONException e) {
+              //  e.printStackTrace();
+            //}
+            }
+			
+		}
+		
+		groupAdapter= new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, groupName);
+		//groupAdapter.
+		list_groups.setAdapter(groupAdapter);
+		list_groups.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Log.e("list", new Integer(position).toString());
+				globalPosition=position;
+				getPopupWindow();
+			}
+		});
+		//Log.e("UserDetails",groupNotifications[0]);
+	}
+	
+	private void getGroupMembers() throws JSONException{
+		ServerRequest sr = new ServerRequest();
+		 params1 = new ArrayList<NameValuePair>();
+       params1.add(new BasicNameValuePair("groupToken", user.getJSONArray("groupRequests").getString(globalPosition)));
+      JSONObject json = sr.getJSON(AppSettings.SERVER_IP+"/getGroupDetails",params1);
+      if(json!=null){
+   	   groupMembers= new String[json.getJSONArray("groupMembers").length()];
+   	   for(int j=0;j<json.getJSONArray("groupMembers").length();j++){
+   		   groupMembers[j]=json.getJSONArray("groupMembers").getString(j);
+   	   }
+      }
 	}
 	
 	@Override
